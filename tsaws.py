4d2968c79cb845179be8c53006217439d7731843
Jeff Bryner <jeff@jeffbryner.com>
2015-04-29 16:17:12 -0700

    add routines to snapshot, create volume and attach to forensic instance

--- tsaws.py
+++ tsaws.py
@@ -14,12 +14,14 @@ import logging
 import os
 import pytz
 import sys
+from configlib import getConfig, OptionParser
 from datetime import datetime
 from datetime import timedelta
-from configlib import getConfig, OptionParser
+from dateutil.parser import parse
 from logging.handlers import SysLogHandler
 from pythonjsonlogger import jsonlogger
-from dateutil.parser import parse
+from time import sleep
+
 logger = logging.getLogger(sys.argv[0])
 
 
@@ -38,6 +40,7 @@ def initLogger():
     sh = logging.StreamHandler(sys.stderr)
     sh.setFormatter(formatter)
     logger.addHandler(sh)
+
 
 def toUTC(suspectedDate, localTimeZone=None):
     '''make a UTC date out of almost anything'''
@@ -84,7 +87,7 @@ def initConfig():
             options.regions.append(r.name)
 
     #target instance
-    #anything in the conf file? 
+    #anything in the conf file?
     options.instances = getConfig('instances',
                                   options.instances,
                                   options.configfile)
@@ -92,7 +95,7 @@ def initConfig():
         options.instances = [x for x in list(options.instances.split(',')) if len(x)>0 ]
 
     #target volumes?
-    #anything in the conf file? 
+    #anything in the conf file?
     options.volumes = getConfig('instances',
                                   options.volumes,
                                   options.configfile)
@@ -107,7 +110,7 @@ def get_ec2_instances(region):
         for r in reservations:
             logger.info('Region: {0} {1} {2} instances'.format(region, r, len(r.instances)))
             for i in r.instances:
-                logger.info('Instances: id: {0} state: {1} image_id: {2} root_device: {3} tags: {4}'.format(i.id, i.state, i.image_id,i.root_device_type,i.tags))
+                logger.info('Instances: id: {0} state: {1} image_id: {2} root_device: {3} tags: {4} zone: {5}'.format(i.id, i.state, i.image_id,i.root_device_type,i.tags, i.placement))
 
         #for vol in ec2_conn.get_all_volumes():
         #    print region+':',vol.id
@@ -117,18 +120,16 @@ def get_ec2_instances(region):
         pass
     except Exception as e:
         logger.error('Exception: {0}'.format(e))
-        pass    
+        pass
 
 
 def get_instance_info(region, instances):
     ec2_conn = boto.ec2.connect_to_region(region)
-    logger.debug('looking for {0} in {1}'.format(instances, region))
+    logger.info('looking for {0} in {1}'.format(instances, region))
     try:
         for i in ec2_conn.get_only_instances(instance_ids=instances):
-            print(i.id)
-            #m = get_instance_metadata()
             volumes = [v for v in ec2_conn.get_all_volumes() if v.attach_data.instance_id == i.id]
-            print('Instance volumes:{0}'.format(volumes))
+            logger.info('Instance volumes:{0}'.format(volumes))
 
     except boto.exception.EC2ResponseError as e:
         # instance not found in this region, move on.
@@ -139,24 +140,91 @@ def get_instance_info(region, instances):
         pass
 
 
+def list_volumes(region):
+    ec2_conn = boto.ec2.connect_to_region(region)
+    volumes = [v for v in ec2_conn.get_all_volumes()]
+    for v in volumes:
+        logger.info('volume {0} created: {1} size: {2} state:{3} tags:{4}'.format(v.id, v.create_time, v.size, v.volume_state(), v.tags))
+
+
+def list_snapshots(region):
+    ec2_conn = boto.ec2.connect_to_region(region)
+    snapshots = [s for s in ec2_conn.get_all_snapshots()]
+    for s in snapshots:
+        logger.info('snapshots {0} created: {1} size: {2} description: {3} '.format(s.id, s.start_time, s.volume_size, s.description))
+        logger.debug(s.zone)
+
+
 def snapshot_volumes(region):
     ec2_conn = boto.ec2.connect_to_region(region)
     volumes = [v for v in ec2_conn.get_all_volumes() if v.id in options.volumes]
     for v in volumes:
-        s = v.create_snapshot(description='forensic snapshot of {0} taken {1}'.format(v.attach_data.instance_id, toUTC(datetime.now()).isoformat()))
-        print('created {0}'.format(s))
+        s = v.create_snapshot(description='forensic snapshot of {0} taken {1}'.format(v.attach_data.instance_id, datetime.utcnow().isoformat()))
+        logger.info('created {0}'.format(s))
+
+
+def attach_snapshot(region):
+    ec2_conn = boto.ec2.connect_to_region(region)
+    # get the forensic instance first to
+    # get the availability zone to use when creating the volume
+    if options.forensic:
+        for i in ec2_conn.get_only_instances(instance_ids=options.forensic):
+            logger.debug('Using forensic image: {0} zone:{1}'.format(i.id, i.placement))
+
+            if i.state not in ['stopped']:
+                logger.info('Instance not stopped..stopping')
+                i.stop()
+                logger.info('Finished the ask to stop...monitoring')
+                while i.state not in ['stopped']:
+                    sleep(2)
+                    i.update(True)
+                logger.info('State:{0}'.format(i.state))
+            logger.debug('Using forensic image: {0} zone:{1}'.format(i.id, i.placement))
+            snapshots = [s for s in ec2_conn.get_all_snapshots() if s.id in options.snapshots]
+            for s in snapshots:
+                logger.debug('Creating volume for  forensic image: {0} zone:{1}'.format(i.id, i.placement))
+                snapshot_volume = s.create_volume(i.placement)
+                while snapshot_volume.volume_state() not in ['available']:
+                    sleep(2)
+                    snapshot_volume.update(True)
+
+                logger.debug(snapshot_volume)
+
+                if snapshot_volume.attach(i.id, '/dev/sdf'):
+                    logger.info('Attached {0} to instance {1}'.format(snapshot_volume.id, i.id))
 
 
 def main():
-    if options.regions or options.instances:
+    if options.regions:
         for region in options.regions:
-            if options.instances is None or len(options.instances) == 0:
+
+            # list instances in a region?
+            if (options.instances is None  and options.volumes is None) \
+               or (options.instances and 'list' in options.instances):
                 get_ec2_instances(region)
-            else:
+
+            # target a specific instance?
+            if options.instances \
+               and len(options.instances) > 0 \
+               and options.action == "info":
                 get_instance_info(region, options.instances)
 
-            if options.volumes is not None and len(options.volumes) > 0:
-                snapshot_volumes(region)
+            # target a specific volume
+            if options.volumes and len(options.volumes) > 0:
+                if options.action == "snapshot":
+                    snapshot_volumes(region)
+                if options.action == "list":
+                    list_volumes(region)
+
+            # target a specific snapshot
+            if options.snapshots and len(options.snapshots) > 0:
+                # attach a snapshot to forensics?
+                if options.action == 'attach' and \
+                   options.snapshots and \
+                   options.forensic :
+                    attach_snapshot(region)
+                if options.action == 'list':
+                    list_snapshots(region)
 
 
 
@@ -166,7 +234,10 @@ if __name__ == '__main__':
     parser.add_option("-o", "--output", dest='output', default='text', help="output format, json or text")
     parser.add_option("-r", "--regions", dest='regions', default=None, help="comma delimited list of regions to target")
     parser.add_option("-i", "--instances", dest='instances', default=None, help="comma delimited list of instance IDs to target")
-    parser.add_option("-v", "--volumes", dest='volumes', default=None, help="comma delimited list of volume IDs to snapshot")
+    parser.add_option("-f", "--forensic", dest='forensic', default=None, help="instance IDs to use as the forensic workstation")
+    parser.add_option("-v", "--volumes", dest='volumes', default=None, help="comma delimited list of volume IDs to target")
+    parser.add_option("-s", "--snapshots", dest='snapshots', default=None, help="comma delimited list of snapshot IDs to attach to the forensic instance")
+    parser.add_option("-a", "--action", dest='action', default='list', type="choice", choices=["list", "info", "snapshot", "attach"], help="Action to perform, defaults to list")
     (options, args) = parser.parse_args()
     initConfig()
     initLogger()
